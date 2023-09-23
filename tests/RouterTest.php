8ed3b987251f8e7f49c24fb42a70e1133c27ece3
jordihomedes24 <jordihomedes48@gmail.com>
2023-09-23 18:53:19 +0200

    Add router tests

---
+++ tests/RouterTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Lune\Tests;
+
+use Lune\HttpMethod;
+use Lune\Router;
+use PHPUnit\Framework\TestCase;
+
+class RouterTest extends TestCase {
+    public function test_resolve_basic_route_with_callback_action() {
+        $uri = "/test";
+        $action = fn () => "THIS IS A TEST";
+        $router = new Router();
+        $router->get($uri, $action);
+
+        $this->assertEquals($action, $router->resolve($uri, HttpMethod::GET->value));
+    }
+
+    public function test_resolve_multiple_basic_route_with_callback_action() {
+        $routes = [
+            '/test' => fn () => "test",
+            '/foo' => fn () => "foo",
+            '/bar' => fn () => "bar",
+            '/long/nested/route' => fn() => "long nested route",
+        ];
+
+        $router = new Router();
+        
+        foreach($routes as $uri => $action) {
+            $router->get($uri, $action);
+        }
+
+        foreach($routes as $uri => $action) {
+            $this->assertEquals($action, $router->resolve($uri, HttpMethod::GET->value));
+        }
+    }
+
+    public function test_resolve_multiple_basic_route_with_callback_action_for_different_http_methods() {
+        $routes = [
+            [HttpMethod::GET, "/test", fn () => "get"],
+            [HttpMethod::POST, "/test", fn () => "post"],
+            [HttpMethod::PUT, "/test", fn () => "put"],
+            [HttpMethod::PATCH, "/test", fn () => "patch"],
+            [HttpMethod::DELETE, "/test", fn () => "delete"],
+
+            [HttpMethod::GET, "/random/get", fn () => "get"],
+            [HttpMethod::POST, "/more/random/post", fn () => "post"],
+            [HttpMethod::PUT, "/some/put/random", fn () => "put"],
+            [HttpMethod::PATCH, "/hello/this/is/patch", fn () => "patch"],
+            [HttpMethod::DELETE, "/a/delete", fn () => "delete"],
+        ];
+
+        $router = new Router();
+
+        foreach($routes as [$method, $uri, $action]) {
+            $router->{strtolower($method->value)}($uri, $action);
+        };
+
+        foreach($routes as [$method, $uri, $action]) {
+            $this->assertEquals($action, $router->resolve($uri, $method->value));
+        };
+    }
+}
