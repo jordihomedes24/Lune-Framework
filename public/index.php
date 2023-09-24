e04dbe9cdeb0e2c28ce04d2e130ed1170ac54c02
jordihomedes24 <jordihomedes48@gmail.com>
2023-09-24 17:03:22 +0200

    Routes with parameters

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 require_once "../vendor/autoload.php";
 
 use Lune\HttpNotFoundException;
+use Lune\Route;
 use Lune\Router;
 
 $router = new Router();
@@ -11,8 +12,13 @@ $router->get('/test', function () {
     return "GET OK";
 });
 
+$router->post('/test', function () {
+    return "POST OK";
+});
+
 try {
-    $action = $router->resolve($_SERVER["REQUEST_URI"], $_SERVER["REQUEST_METHOD"]);
+    $route = $router->resolve($_SERVER["REQUEST_URI"], $_SERVER["REQUEST_METHOD"]);
+    $action = $route->action();
     print($action());
 } catch (HttpNotFoundException $e) {
     print("NOT FOUND");
